8e4dbc71019e3eef6e77377afac3673c8dfd212f
manana <manana@Core11-24>
2018-01-10 10:52:52 +0100

    Primera Versión

---
+++ 14_dado/src/dado/DadoElectronico.java
@@ -0,0 +1,21 @@
+package dado;
+
+public class DadoElectronico {
+
+	public static void main(String[] args) {
+		int[] contadores = new int[6];
+		final int tiradas=10000000;
+		for (int i=0;i<tiradas;i++) {
+			int dado= (int)Math.floor(Math.random()*6+1);
+			contadores[dado-1]++;
+			
+		}
+		for(int i=0;i<contadores.length;i++) {
+			System.out.println("Porcentaje de "+(i+1)+":"+ contadores[i]*100.0/tiradas);
+		}
+	}
+		
+	
+	
+
+}
