18bc3e0f50b0d6b7f0da8d8ddbf147bab9cfa45d
Azzurra <Azzurra@DESKTOP-QCSN49C>
2018-01-10 21:24:12 +0100

    Primer Paso

---
+++ loteria/src/loteria/Loteria.java
@@ -0,0 +1,26 @@
+package loteria;
+
+public class Loteria {
+
+	public static void main(String[] args) {
+		
+	
+	int contador=0;
+	int [] combinacion =new int [6];
+
+	while(contador<=5) {
+		int numero=(int)(Math.ceil(Math.random()*48))+1;
+		if ((numero!=combinacion[5])&&(numero!=combinacion[4])&&(numero!=combinacion[3])&&(numero!=combinacion[2])&&(numero!=combinacion[1])&&(numero!=combinacion[0])) {
+			combinacion[contador]=numero;
+			contador++;
+			}
+		}	
+	System.out.println("\nEl resultado de la primitiva es: ");
+	for(int n:combinacion) {
+		System.out.print(" "+n);
+		}
+	}
+}
+	
+
+
